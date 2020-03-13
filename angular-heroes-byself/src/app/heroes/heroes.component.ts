53b7a2ba6f7eeb478de34731edcb6ef971334814
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-03-12 22:07:27 -0400

    3 hero tutorial

--- angular-heroes-byself/src/app/heroes/heroes.component.ts
+++ angular-heroes-byself/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,16 +9,26 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  // Tutorial - 1
   // hero = 'Sean Huang';
-  hero: Hero = {
-    id: 1,
-    name: 'Sean Huang'
-  }
+
+  // Tutorial - 2
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Sean Huang'
+  // }
+
+  // Tutorial - 3
+  heroes = HEROES;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Tutorial - 3 method
+  selectedHero: Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
---
+++ angular-heroes-byself/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Mr. Huang'},
+    { id: 2, name: 'Sean'},
+    { id: 3, name: 'Hsuan'},
+    { id: 4, name: 'Miss Lai'},
+    { id: 5, name: 'Winnie'},
+    { id: 6, name: 'Ju'},
+    { id: 7, name: 'MU'},
+    { id: 8, name: 'Mrs. Lin'},
+    { id: 9, name: 'Mrs. Lee'},
+    { id: 10, name: 'Toronto'}
+];
