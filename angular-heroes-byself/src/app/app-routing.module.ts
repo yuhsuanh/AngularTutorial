08317bd953754658c4faf2089ce6d42427aa2fb5
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-03-14 09:57:04 -0400

    6-1 hero tutorial

---
+++ angular-heroes-byself/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { RouterModule, Router, Routes } from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+]
+
+@NgModule({
+  // declarations: [],
+  // imports: [
+  //   // CommonModule
+  //   RouterModule
+  // ]
+
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+
+})
+export class AppRoutingModule {}
