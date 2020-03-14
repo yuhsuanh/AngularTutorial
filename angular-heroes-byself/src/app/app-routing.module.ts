d0633e652825a71c8542e884b9fbdbeba40bf9fc
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-03-14 10:54:14 -0400

    6-3 hero tutorial

--- angular-heroes-byself/src/app/app-routing.module.ts
+++ angular-heroes-byself/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Router, Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
 ]
 
 @NgModule({
